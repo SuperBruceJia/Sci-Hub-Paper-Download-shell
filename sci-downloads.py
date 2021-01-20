a8621ad6f4fa5505bbb4bf319f4b9b6b0304d7d9
Bruce Shuyue JIA <shuyuej.ml@gmail.com>
2021-01-20 14:09:15 +0800

    Update sci-downloads.py

--- sci-downloads.py
+++ sci-downloads.py
@@ -3,15 +3,13 @@
 
 import os
 import sys
-import getpass
 from scihub import SciHub
 
 sh = SciHub()
-user = str(getpass.getuser())
+SAVE_PATH = os.getcwd() + '/Desktop/'
 
 for i in range(1, len(sys.argv)):
     # identifier can be link URL, DOI, or PMID
     identifier = str(sys.argv[i])
     # PDF Saved Path - You should change this to your own path
-    SAVE_PATH = '/Users/' + user + '/Desktop/'
     sh.download(identifier=identifier, path=SAVE_PATH)
