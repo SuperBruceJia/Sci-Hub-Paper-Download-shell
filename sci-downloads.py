7140f030ebacfc7a2ccde14ad5217d006e94a57e
Bruce Shuyue JIA <shuyuej.ml@gmail.com>
2021-01-20 13:13:51 +0800

    Update sci-downloads.py

--- sci-downloads.py
+++ sci-downloads.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
 import sys
 import getpass
 from scihub import SciHub
 
 sh = SciHub()
-user = getpass.getuser()
+user = str(getpass.getuser())
 
-if len(sys.argv) == 2:
-    name = str(sys.argv[1])
-    result = sh.download(name, path='/Users/'+ str(user) + '/Desktop/paper.pdf')
-else:
-    for i in range(1, len(sys.argv)):
-        name = str(sys.argv[i])
-        result = sh.download(name, path='/Users/' + str(user) + '/Desktop/paper' + str(i) + '.pdf')
+for i in range(1, len(sys.argv)):
+    k = i
+    name = str(sys.argv[i])
+    PATH = '/Users/' + user + '/Desktop/paper' + str(i) + '.pdf'
+
+    while os.path.exists(PATH) is True:
+        k += 1
+        PATH = '/Users/' + user + '/Desktop/paper' + str(k) + '.pdf'
+
+    sh.download(name, path=PATH)
